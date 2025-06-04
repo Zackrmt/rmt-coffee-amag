d6ef5c90f143e4c21a2fdd0c9cc285dd8cf1c93e
Zackrmt <intcpy@gmail.com>
2025-06-05 04:12:29 +0800

    Update constants.py

--- config/constants.py
+++ config/constants.py
@@ -1,6 +1,6 @@
 # Current information
 CURRENT_USER = "Zackrmt"
-STARTUP_TIME = "2025-06-04 19:37:49"
+STARTUP_TIME = "2025-06-04 20:11:44"
 
 # Conversation states
 (CHOOSING_MAIN_MENU,
@@ -17,3 +17,16 @@ STARTUP_TIME = "2025-06-04 19:37:49"
  CHOOSING_DESIGN,
  CHOOSING_QUESTION_SUBJECT,
  CONFIRMING_DELETION) = range(14)
+
+# Message Templates
+MESSAGES = {
+    'welcome': 'Welcome to MTLE Study Bot! Choose an option:',
+    'study_start': '📚 {user} started studying {subject}.',
+    'break_start': '☕ {user} started their break.',
+    'break_end': '⏰ {user} ended their break and resumed studying.',
+    'session_end': '📚 {user} ended their review on {subject}. Congrats {user}!',
+    'study_time': 'Your total study time: {hours:02d}:{minutes:02d}',
+    'break_time': 'Your total break time: {hours:02d}:{minutes:02d}',
+    'goal_time': 'Your goal study time was: {goal}',
+    'share_prompt': 'Would you like to create and share a progress image?'
+}
