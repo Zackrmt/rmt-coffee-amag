7248074773d2b1e3c581630e4672e24daf01860c
Zackrmt <intcpy@gmail.com>
2025-06-05 04:09:16 +0800

    Create subjects.py

---
+++ config/subjects.py
@@ -0,0 +1,18 @@
+# Subject emojis and codes
+SUBJECTS = {
+    "CC 🧪": "CC",
+    "BACTE 🦠": "BACTE",
+    "VIRO 👾": "VIRO",
+    "MYCO 🍄": "MYCO",
+    "PARA 🪱": "PARA",
+    "CM 🚽💩": "CM",
+    "HISTO 🧻🗳️": "HISTO",
+    "MT Laws ⚖️": "MT Laws",
+    "HEMA 🩸": "HEMA",
+    "IS ⚛": "IS",
+    "BB 🩹": "BB",
+    "MolBio 🧬": "MolBio",
+    "Autopsy ☠": "Autopsy",
+    "General Books 📚": "General Books",
+    "RECALLS 🤔💭": "RECALLS"
+}
