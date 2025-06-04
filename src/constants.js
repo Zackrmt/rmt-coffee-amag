9c78b427e3129b3e82578faadad0e98cf42e3018
Zackrmt <intcpy@gmail.com>
2025-06-04 09:13:48 +0800

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,33 @@
+const SUBJECTS = {
+    CC: 'CC 🧪',
+    BACTE: 'BACTE 🦠',
+    VIRO: 'VIRO 👾',
+    MYCO: 'MYCO 🍄',
+    PARA: 'PARA 🪱',
+    CM: 'CM 🚽💩',
+    HISTO: 'HISTO 🧻🗳️',
+    MT_LAWS: 'MT Laws ⚖️',
+    HEMA: 'HEMA 🩸',
+    IS: 'IS ⚛',
+    BB: 'BB 🩹',
+    MOLBIO: 'MolBio 🧬',
+    AUTOPSY: 'Autopsy ☠',
+    GENERAL_BOOKS: 'General Books 📚',
+    RECALLS: 'RECALLS 🤔💭'
+};
+
+const ACTIONS = {
+    START_STUDYING: 'START_STUDYING',
+    SELECT_SUBJECT: 'SELECT_SUBJECT',
+    START_BREAK: 'START_BREAK',
+    END_BREAK: 'END_BREAK',
+    END_SESSION: 'END_SESSION',
+    CREATE_QUESTION: 'CREATE_QUESTION',
+    ANSWER_QUESTION: 'ANSWER_QUESTION',
+    DONE_READING: 'DONE_READING'
+};
+
+module.exports = {
+    SUBJECTS,
+    ACTIONS
+};
