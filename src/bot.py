4f8c8fa924579e115c50381ea317ba66a15e7092
Zackrmt <intcpy@gmail.com>
2025-06-20 05:14:26 +0800

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -25,7 +25,8 @@ from telegram.ext import (
     filters,
     ApplicationBuilder,
     PersistenceInput,
-    PicklePersistence
+    PicklePersistence,
+    TypeHandler
 )
 from telegram.error import Conflict
 
